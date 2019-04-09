61e8379b5cc702ac9babadcab37144998a42878b
mrdanortiz <dortiz357@gmail.com>
2019-04-08 19:21:45 -0500

    refactored familyA tests and added additional tests

--- src/calculators/familyACalculations.spec.ts
+++ src/calculators/familyACalculations.spec.ts
@@ -1,7 +1,40 @@
 import { familyACalculations } from "./familyACalculations";
 import { totalHoursWorked } from "./totalHoursWorked";
 
-it("calculates the rate for family A if worked from 5pm-1am", () => {
-    const totalHours = totalHoursWorked(17, 1);
-    expect(familyACalculations(totalHours)).toEqual(130);
-});
+const tests = [
+    {
+        startTime: "5:00 PM",
+        endTime: "2:00 AM",
+        startHour: 17,
+        endHour: 2,
+        result: 150
+    },
+    {
+        startTime: "6:00 PM",
+        endTime: "9:00 PM",
+        startHour: 18,
+        endHour: 21,
+        result: 45
+    },
+    {
+        startTime: "9:00 PM",
+        endTime: "4:00 AM",
+        startHour: 21,
+        endHour: 4,
+        result: 130
+    },
+    {
+        startTime: "1:00 AM",
+        endTime: "3:00 AM",
+        startHour: 1,
+        endHour: 3,
+        result: 40
+    }
+];
+
+tests.map((test) =>
+    it(`calculates the rate for family A if worked from ${test.startTime} - ${test.endTime}`, () => {
+        const totalHours = totalHoursWorked(test.startHour, test.endHour);
+        expect(familyACalculations(totalHours)).toEqual(test.result);
+    })
+);
